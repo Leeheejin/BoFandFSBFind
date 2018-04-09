bd13fd003816121cc31282dd8933c8ccb25c5469
Leeheejin <leinaere@naver.com>
2018-04-08 18:42:14 -0700

    bof done

--- bof.c
+++ bof.c
@@ -68,7 +68,7 @@ int chk_license()
 void license()
 {
 	char buf[4]; //yes 와 no로 정확한 입력을 받기 위하여 buf 공간을 16에서 4로 수정합니다.
-	memset(buf,0,3); // 마찬가지로 초기화도 buf 공간의 16까지가 아닌 4까지 초기화합니다.
+	memset(buf,0,sizeof(buf)); // 마찬가지로 초기화도 buf 공간의 16까지가 아닌 4까지 초기화합니다.
 
 	if(is_license_agree == 1)
 	{
@@ -86,7 +86,7 @@ void license()
 	puts("########################################");
 	printf("Do you agree\?\?(yes/no)");
 	fflush(stdout);
-	fgets(buf,sizeof(buf)-1,stdin); // 256에서 sizeof(buf)-1 으로 입력받을 공간을 제한합니다.
+	fgets(buf,sizeof(buf)-1,stdin); // 입력받을 공간 크기를 256에서 sizeof(buf)-1 으로 제한합니다.
 	
 	if(sizeof(buf) > 4) // 입력이 제대로 들어왔다면 버퍼를 비우기 위해서 \n을 한번 더 입력하지 않아도 됩니다.
 		while(getchar() != '\n') {getchar();}
@@ -112,7 +112,7 @@ void reg_name(INFO *myinfo)
 		printf(">>> ");
 		fflush(stdout);
 		getchar();
-		scanf("%16s",myinfo->name); // 입력 최대 길이를 제한하여 버퍼오버플로우를 막습니다. %s -> %16s
+		scanf("%15s",myinfo->name); // 변환명세 변경을 통해 입력 최대 길이를 제한하여 버퍼오버플로우를 막습니다. %s -> %16s
 		printf("Hello %s\n",myinfo->name);
 	}
 }
@@ -129,7 +129,7 @@ void reg_phone(INFO *myinfo)
 		printf(">>> ");
 		fflush(stdout);
 		getchar();
-		fgets(buf,15,stdin); // 전화번호를 받을 공간을 256에서 buf 배열의 크기보다 하나 작은만큼 입력을 받게 15로 제한하는 것으로 수정합니다. 16이 아닌 이유는 string의 끝 부분을 알아야 해서 \n이 들어갈 공간을 남겼습니다.
+		fgets(buf,sizeof(buf) - 1,stdin); // 전화번호를 받을 공간을 256에서 buf 배열의 크기보다 하나 작은만큼 입력을 받게 15로 제한하는 것으로 수정합니다. 16이 아닌 이유는 string의 끝 부분을 알아야 해서 \n이 들어갈 공간을 남겼습니다.
 		buf[strlen(buf)] = '\x00'; //strlen(buf)-1 에서 수정하였습니다.
 		strncpy(myinfo->phone,buf,16);
 		puts("Phone number Registration Complete!");
